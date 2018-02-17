0269683f92f169510d725fb689545fd924e411be
Kamarudeen O. Ayankunbi <koa15@factory.cs.rutgers.edu>
2018-02-17 18:34:54 -0500

    First commit

--- memgrind.c
+++ memgrind.c
@@ -3,8 +3,27 @@
 #include "my_malloc.h"
 #include<sys/time.h>
 
+double testA(){
+struct timeval start, end;
+gettimeofday(&start, NULL);
+
+int i=0;
+for(;i < 150; i++){
+	char * a = malloc(sizeof(char));
+	free(a);
+		}
+gettimeofday(&end, NULL);
+printf("time: %lu seconds and %lu milliseconds\n", (end.tv_sec-start.tv_sec), ((end.tv_usec-start.tv_usec)));
+}
+double testB(){
+static char myblock[5000];
+
+
+}
+
 int main()
 {
+
 	struct timeval start, end;
 
 	gettimeofday(&start, NULL);
